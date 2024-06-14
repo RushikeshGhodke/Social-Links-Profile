3709acbc04cb1446ef1dd4557e4f842616d68b51
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2024-06-14 13:03:53 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById("portfolio").addEventListener("click", () => {
+    // window.open('https://example.com', '_blank');
+})
+
+document.getElementById("resume").addEventListener("click", () => {
+    // window.open('https://example.com', '_blank');
+})
+
+document.getElementById("linkedin").addEventListener("click", () => {
+    window.open('https://linkedin.com/in/realrushikeshghodke', '_blank');
+})
+
+document.getElementById("github").addEventListener("click", () => {
+    window.open('https://github.com/RushikeshGhodke', '_blank');
+})
+
+document.getElementById("twitter").addEventListener("click", () => {
+    // window.open('https://example.com', '_blank');
+})
+
+document.getElementById("instagram").addEventListener("click", () => {
+    window.open('https://instagram.com/merushikeshghodke/', '_blank');
+})
